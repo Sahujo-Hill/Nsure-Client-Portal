0efaecf0b5de654922d9316a68b742d94ede8221
Josh Hill <148567526+Sahujo-Hill@users.noreply.github.com>
2024-07-10 18:41:13 +0100

    Various Changes
    Code cleanup
    Added hover effects for policy buttons
    Navmenu styling changes

--- NsureProject/obj/Debug/net6.0/NsureProject.AssemblyInfo.cs
+++ NsureProject/obj/Debug/net6.0/NsureProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NsureProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e7f13c8ca46e876fe765be0bde812da0edf949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d828816693d15213dd21d9ad2579dc2c2387fc7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NsureProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NsureProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
